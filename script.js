f1e1e4e2c3e3f45426d6543ffe0f19ace2b9a13f
divakard2920 <104456149+divakard2920@users.noreply.github.com>
2022-04-26 23:12:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter Name");
+ var b=prompt("Enter Date of Birth");
+  	if(a== "Aishwarya" && b=="28-04-2000")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
